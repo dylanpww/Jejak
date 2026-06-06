156186abd51501113ad3c866eec53cf08a8e78b6
dylan <dylanpatrickw@gmail.com>
2026-06-06 14:59:07 +0700

    Initial Commit

---
+++ journal cam/journal_camApp.swift
@@ -0,0 +1,17 @@
+//
+//  journal_camApp.swift
+//  journal cam
+//
+//  Created by Selena Cheryl Willyam on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct journal_camApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
